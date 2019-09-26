df4cc7a84bc3a9dd2f6d8b00111b066916763a46
Christoper Tracey <ctracey@paconserve.org>
2019-09-25 22:14:00 -0400

    add rdata

--- scripts/SGCN_DataCollection/00_PathsAndSettings.r
+++ scripts/SGCN_DataCollection/00_PathsAndSettings.r
@@ -3,9 +3,15 @@
 # load the arcgis license
 arc.check_product() 
 
+# update name
+updateName <- "Fall2019"
+# rdata  file
+updateData <- here::here("_data","output",paste(updateName, "RData", sep="."))
 
 # output database name
 databasename <- here::here("_data","output","coa_bridgetest.sqlite")
+
+# rdata file name
 
 # paths to biotics shapefiles
 biotics_path <- "W:/Heritage/Heritage_Data/Biotics_datasets.gdb"
