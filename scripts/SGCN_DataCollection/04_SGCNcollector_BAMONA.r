c2c8bceb21e04bda0b8414a0815cac1da45b3fd1
Christoper Tracey <ctracey@paconserve.org>
2019-09-25 22:15:05 -0400

    rdata modifications

--- scripts/SGCN_DataCollection/04_SGCNcollector_BAMONA.r
+++ scripts/SGCN_DataCollection/04_SGCNcollector_BAMONA.r
@@ -30,32 +30,47 @@ if (!requireNamespace("sf", quietly = TRUE)) install.packages("sf")
   require(sf)
 if (!requireNamespace("RSQLite", quietly = TRUE)) install.packages("RSQLite")
   require(RSQLite)
+if (!requireNamespace("rmarkdown", quietly = TRUE)) install.packages("rmarkdown")
+  require(rmarkdown)
 
 source(here::here("scripts","SGCN_DataCollection","00_PathsAndSettings.r"))
+
+load(file=updateData)
 
 # read in SGCN data
 loadSGCN()
 lu_sgcn <- lu_sgcn[which(substr(lu_sgcn$ELSeason,1,4)=="IILE"),]
 
 # read in BAMONA data
-bamona <- read.csv(here("_data","input","SGCN_data","bamona","bamona_data_02_19_2019.csv"), stringsAsFactors=FALSE)
+bamona_file <- "bamona_data_02_19_2019.csv"
+bamona <- read.csv(here("_data","input","SGCN_data","bamona", bamona_file), stringsAsFactors=FALSE)
 bamona_citation <- "Lotts, Kelly and Thomas Naberhaus, coordinators. 2017. Butterflies and Moths of North America. http://www.butterfliesandmoths.org/ (Version MMDDYYYY)"
+
+bamona_backup <- bamona
+
+# change field names
+names(bamona)[names(bamona)=='Scientific.Name'] <- 'SNAME'
+names(bamona)[names(bamona)=='Common.Name'] <- 'SCOMNAME'
+names(bamona)[names(bamona)=='Record.Number'] <- 'DataID'
+names(bamona)[names(bamona)=='Lat.Long'] <- 'Latitude'
 
 # prune bad data from master BAMONA
 bamona <- bamona[which(!is.na(bamona$Longitude)),]
-bamona$year <- year(parse_date_time(bamona$Observation.Date,"mdy"))
-bamona <- bamona[which(!is.na(bamona$year)),]
+bamona$LastObs <- year(parse_date_time(bamona$Observation.Date,"mdy"))
+bamona <- bamona[which(!is.na(bamona$LastObs)),]
 
 #subset BAMONA data by SGCN
-bamona1 <- bamona[bamona$Scientific.Name %in% lu_sgcn$SNAME,]
-print(paste(length(unique(bamona1$Scientific.Name)),"of the", length(unique(lu_sgcn$SNAME)) ,"lep SGCN were found in the BAMONA database"), sep=" ")
+bamona1 <- bamona[bamona$SNAME %in% lu_sgcn$SNAME,]
+print(paste(length(unique(bamona1$SNAME)),"of the", length(unique(lu_sgcn$SNAME)) ,"lep SGCN were found in the BAMONA database"), sep=" ")
 
 # get a list of SGCN not found in the bamona database
 NotInBamona <- setdiff(lu_sgcn$SNAME, bamona1$Scientific.Name)
-
+NotInBamona
 # subset to leps that are not in Biotics
-SGCN_bioticsCPP <- read.csv("SGCN_bioticsCPP.csv", stringsAsFactors=FALSE)
-bamona1 <- bamona1[which(!bamona1$Scientific.Name %in% SGCN_bioticsCPP$x),]
+#DELETE SGCN_bioticsCPP <- read.csv("SGCN_bioticsCPP.csv", stringsAsFactors=FALSE)
+bamona1 <- bamona1[which(!bamona1$SNAME %in% SGCN_bioticsCPP),]
+
+table(bamona1$SNAME)
 
 # add in useCOA
 bamona1$UseCOA <- NA
@@ -66,11 +81,6 @@ bamona1$UseCOA <- with(bamona1, ifelse(bamona1$year >= cutoffYear, "y", "n"))
 bamona1$DataSource <- "BAMONA"
 bamona1$SeasonCode <- "y"
 bamona1$OccProb <- "k"
-names(bamona1)[names(bamona1)=='Scientific.Name'] <- 'SNAME'
-names(bamona1)[names(bamona1)=='Common.Name'] <- 'SCOMNAME'
-names(bamona1)[names(bamona1)=='Record.Number'] <- 'DataID'
-names(bamona1)[names(bamona1)=='Lat.Long'] <- 'Latitude'
-names(bamona1)[names(bamona1)=='year'] <- 'LastObs'
 
 # delete the colums we don't need from the BAMONA dataset
 bamona1 <- bamona1[c("DataSource","DataID","SNAME","Longitude","Latitude","OccProb","LastObs","SeasonCode","UseCOA")]
@@ -89,3 +99,6 @@ arc.write(path=here("_data/output/SGCN.gdb","final_BAMONA"), bamona_sf, overwrit
 
 # clean up
 rm(bamona, bamona1, lu_sgcn, SGCN_bioticsCPP)
+
+
+#render(here("scripts","SGCN_DataCollection","markdown_BAMONA.rmd"))
