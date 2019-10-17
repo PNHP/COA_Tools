09691ea956ed3650be21cc62bb993389704290ff
Molly Moore <mollymoore22@gmail.com>
2019-10-17 09:13:06 -0400

    review updates

--- scripts/SGCN_DataCollection/20_SGCNreview.r
+++ scripts/SGCN_DataCollection/20_SGCNreview.r
@@ -20,25 +20,40 @@ if (!requireNamespace("sf", quietly = TRUE)) install.packages("sf")
 require(sf)
 if (!requireNamespace("RSQLite", quietly = TRUE)) install.packages("RSQLite")
 require(RSQLite)
+if (!requireNamespace("pingr", quietly = TRUE)) install.packages("pingr")
+require(pingr)
 
 source(here::here("scripts","SGCN_DataCollection","00_PathsAndSettings.r"))
 
 arc.check_product()
 
 ######################################################################################
-# get SGCN data
+# get lu_sgcn and lu_sgcnXpu data from sqlite database
 db <- dbConnect(SQLite(), dbname = databasename)
 lu_sgcn_SQLquery <- "SELECT ELSeason, ELCODE, SCOMNAME, SNAME, TaxaDisplay, SeasonCode FROM lu_sgcn"
 lu_sgcnXpu_SQLquery <- "SELECT unique_id, OccProb, PERCENTAGE, ELSeason FROM lu_sgcnXpu_all"
+lu_actionsLevel2_SQLquery <- "SELECT ELSeason, SCOMNAME, SNAME, ActionCategory2, COATool_ActionsFINAL FROM lu_actionsLevel2" #eventually add Group - currently not working
 lu_sgcn <- dbGetQuery(db, statement = lu_sgcn_SQLquery)
 lu_sgcnXpu <- dbGetQuery(db, statement = lu_sgcnXpu_SQLquery)
+lu_actions <- dbGetQuery(db, statement = lu_actionsLevel2_SQLquery)
 dbDisconnect(db) # disconnect the db
 
+# get number of lu_sgcn records, number of unique ELCODE values, and number of unique ELSeason values
 print(paste0("There are ",nrow(lu_sgcn)," rows in the master lu_sgcn list from the sqlite database."))
 print(paste0("There are ",length(unique(lu_sgcn$ELSeason))," unique ELSeason values in the master lu_sgcn list from the sqlite database."))
 print(paste0("There are ",length(unique(lu_sgcn$ELCODE))," unique ELCODE values in the master lu_sgcn list from the sqlite database."))
 
-dupe = lu_sgcn[,c('ELSeason')] # select columns to check duplicates
+# check to make sure base elcode and elseason values match in lu_sgcn table
+elcode_matching <- lu_sgcn[sub("\\_.*","",lu_sgcn$ELSeason) != lu_sgcn$ELCODE,]
+if(nrow(elcode_matching)>0){
+  print('There are records for which the base ELCODE and ELSeason values do not match. These include: ')
+  print(elcode_matching)
+} else{
+  print('All base ELCODE and ELSeason values match! Hooray!')
+}
+
+# check for duplicate ELSeason values and print rows containing dup records
+dupe <- lu_sgcn[,c('ELSeason')] # select columns to check duplicates
 dups <- lu_sgcn[duplicated(dupe) | duplicated(dupe, fromLast=TRUE),]
 if(nrow(dups)>0){
   print('There are duplicate ELSeason records in the master lu_sgcn list from the sqlite database. They include: ')
@@ -47,20 +62,59 @@ if(nrow(dups)>0){
   print('There are no duplicate ELSeason records in the master lu_sgcn list from the sqlite database')
 }
 
-
+# get number of records in lu_sgcnXpu table and unique ELSeason values in lu_sgcnXpu table
 print(paste0("There are ",nrow(lu_sgcnXpu)," records in the lu_sgcnXpu table from the sqlite database."))
 print(paste0("There are ",length(unique(lu_sgcn$ELSeason))," unique ELSeason values in the lu_sgcnXpu table from the sqlite database."))
 
-lu_sgcn_ELSeason <- unique(lu_sgcn$ELSeason)
-lu_sgcnXpu_ELSeason <- unique(lu_sgcnXpu$ELSeason)
+# check for missing ELSeason values from tables
+x <- unique(lu_sgcn$ELSeason)
+y <- unique(lu_sgcnXpu$ELSeason)
+a <- unique(lu_actions$ELSeason)
 
-setdiff(lu_sgcn_ELSeason,lu_sgcnXpu_ELSeason)
-setdiff(lu_sgcnXpu_ELSeason,lu_sgcn_ELSeason)
+# get ELSeason values that are in lu_sgcn table, but not spatially represented in lu_sgcnXpu table
+sgcn_nospatial <- setdiff(x,y)
+print("The following ELSeason records are found in the lu_sgcn table, but are not spatially represented in the lu_sgcnXpu table: ")
+print(lu_sgcn[lu_sgcn$ELSeason %in% sgcn_nospatial,])
+
+# get ELSeason values that are in lu_sgcnXpu table, but do not have a matching ELSeason record in lu_sgcn
+sgcn_norecord <- setdiff(y,x)
+print("The following ELSeason records are found in the lu_sgcnXpu table, but do not have matching records in the lu_sgcn table: ")
+print(sgcn_norecord)
+
+# print ELSeason values that are in actions table, but not in lu_sgcn table
+sgcn_actionnorecord <- setdiff(a,x)
+print("The following ELSeason records are found in the lu_actions table, but do not have matching records in the lu_sgcn table: ")
+print(sgcn_actionnorecord)
+
+# print ELSeason values that are in lu_sgcnXpu and in lu_sgcn, but are not in actions table
+sgcn_punoaction <- setdiff(y,a)
+sgcn_recordnoaction <- subset(sgcn_punoaction,!(sgcn_punoaction %in% sgcn_norecord))
+print("The following ELSeason records are found in the lu_sgcnXpu and lu_sgcn tables, but do not have any corresponding actions: ")
+print(lu_sgcn[lu_sgcn$ELSeason %in% sgcn_recordnoaction,])
+
+# get list of rows from actions table that include null or blank ELSeason values
+actions_null_ELCODE <- lu_actions[is.na(lu_actions$ELSeason)==TRUE,]
+if(nrow(actions_null_ELCODE)>0){
+  print('There are null or blank ELSeason values in the actions table from the sqlite database. They include: ')
+  print(actions_null_ELCODE[,names(actions_null_ELCODE)!="COATool_ActionsFINAL"])
+} else{
+  print('There are no null or blank ELSeason values in the actions table from the sqlite database! Yay!')
+}
+
+print(unique(sort(lu_actions$ActionCategory2)))
+
+
+
+
+
+
+
+
+
 
 
 SGCN <- arc.open(path=here::here("_data/output/SGCN.gdb","allSGCNuse2"))
 SGCN <- arc.select(SGCN)
-
 
 SGCNrecordcount <- aggregate(LastObs~SNAME+DataSource, data=SGCN, FUN=length)
 colnames(SGCNrecordcount)[colnames(SGCNrecordcount)=="LastObs"] <- "RecordCount"
