87f953b72dbeb40ca7b27435a09cbd9777c33c46
Christoper Tracey <ctracey@paconserve.org>
2019-10-21 11:16:07 -0400

    fish changes, made an error

--- scripts/SGCN_DataCollection/08_SGCNcollector_BrookTrout.r
+++ scripts/SGCN_DataCollection/08_SGCNcollector_BrookTrout.r
@@ -21,7 +21,7 @@ source(here::here("scripts","00_PathsAndSettings.r"))
 # read in SGCN data
 loadSGCN("AF")
 
-brooktrout <- arc.open(here("_data","input","SGCN_data","PFBC_BrookTrout","PA_WildTrout_BrookTroutOnly_NR_Mar2016Export.shp")) 
+brooktrout <- arc.open(here::here("_data","input","SGCN_data","PFBC_BrookTrout","PA_WildTrout_BrookTroutOnly_NR_Mar2016Export.shp")) 
 brooktrout <- arc.select(brooktrout, c("SSB"))
 brooktrout <- arc.data2sf(brooktrout)
 
@@ -40,10 +40,6 @@ brooktrout <- brooktrout[final_fields]
 
 # create a spatial layer
 brooktrout_sf <- st_transform(brooktrout, crs=customalbers) # reproject to custom albers
-arc.write(path=here("_data/output/SGCN.gdb","srcln_brooktrout_sf"), brooktrout_sf, overwrite=TRUE) # write a feature class to the gdb
+arc.write(path=here::here("_data/output/SGCN.gdb","srcln_brooktrout_sf"), brooktrout_sf, overwrite=TRUE) # write a feature class to the gdb
 brooktrout_buffer_sf <- st_buffer(brooktrout_sf, 100) # buffer the points by 100m
-arc.write(path=here("_data/output/SGCN.gdb","final_BBAptct"), brooktrout_buffer_sf, overwrite=TRUE) # write a feature class to the gdb
-
-# write a feature class to the gdb
-arc.write(path=here("_data/output/SGCN.gdb","final_brooktrout"), brooktrout, overwrite=TRUE)
-
+arc.write(path=here::here("_data/output/SGCN.gdb","final_brooktrout"), brooktrout_buffer_sf, overwrite=TRUE) # write a feature class to the gdb
