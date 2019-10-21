d729899503c5b20467f9815382be413044a1d735
Christoper Tracey <ctracey@paconserve.org>
2019-10-21 11:02:04 -0400

    misc bugs

--- scripts/SGCN_DataCollection/06_SGCNcollector_RuffedGrouse.r
+++ scripts/SGCN_DataCollection/06_SGCNcollector_RuffedGrouse.r
@@ -23,12 +23,12 @@ loadSGCN("AB")
 
 # read in the grouse data 
 #get the threats template
-grouse_file <- list.files(path=here("_data/input/SGCN_data/PGC_Grouse"), pattern=".shp$")  # --- make sure your excel file is not open.
+grouse_file <- list.files(path=here::here("_data/input/SGCN_data/PGC_Grouse"), pattern=".shp$")  # --- make sure your excel file is not open.
 grouse_file
 #look at the output and choose which shapefile you want to run
 #enter its location in the list (first = 1, second = 2, etc)
 n <- 1
-grouse_file <- here("_data/input/SGCN_data/PGC_Grouse", grouse_file[n])
+grouse_file <- here::here("_data/input/SGCN_data/PGC_Grouse", grouse_file[n])
 
 grouse_file <- arc.open(grouse_file)
 grouse_file <- arc.select(grouse_file)
@@ -41,7 +41,7 @@ grouse_sf$SNAME <- "Bonasa umbellus"
 grouse_sf$SCOMNAME <- "Ruffed Grouse"
 
 ### assign a migration date to each ebird observation.
-birdseason <- read.csv(here("scripts","SGCN_DataCollection","lu_eBird_birdseason.csv"), colClasses = c("character","character","integer","integer"),stringsAsFactors=FALSE)
+birdseason <- read.csv(here::here("scripts","SGCN_DataCollection","lu_eBird_birdseason.csv"), colClasses = c("character","character","integer","integer"),stringsAsFactors=FALSE)
 
 grouse_sf$season <- NA
 for(i in 1:nrow(birdseason)){
@@ -49,7 +49,7 @@ for(i in 1:nrow(birdseason)){
   season <- birdseason[i,2]
   startdate <- birdseason[i,3]
   enddate <- birdseason[i,4]
-  grouse_sf$season[grouse_sf$SCOMNAME==comname & grouse_sf$dayofyear>startdate & grouse_sf$dayofyear<enddate] <- substr(as.character(season), 1, 1)
+  grouse_sf$season[grouse_sf$SCOMNAME==comname & grouse_sf$dayofyear>=startdate & grouse_sf$dayofyear<=enddate] <- substr(as.character(season), 1, 1)
 }
 grouse_sf <- grouse_sf[which(!is.na(grouse_sf$season)),]
 grouse_sf$SCOMNAME <- NULL
@@ -70,9 +70,9 @@ grouse_sf$useCOA <- "y"
 
 # create a spatial layer
 grouse_sf <- st_transform(grouse_sf, crs=customalbers) # reproject to the custom albers
-arc.write(path=here("_data/output/SGCN.gdb","srcpt_PGCgrouse"), grouse_sf, overwrite=TRUE) # write a feature class into the geodatabase
+arc.write(path=here::here("_data/output/SGCN.gdb","srcpt_PGCgrouse"), grouse_sf, overwrite=TRUE) # write a feature class into the geodatabase
 grouse_buffer <- st_buffer(grouse_sf, dist=100) # buffer by 100m
-arc.write(path=here("_data/output/SGCN.gdb","final_PGCgrouse"), grouse_buffer, overwrite=TRUE) # write a feature class into the geodatabase
+arc.write(path=here::here("_data/output/SGCN.gdb","final_PGCgrouse"), grouse_buffer, overwrite=TRUE) # write a feature class into the geodatabase
 
 
 
