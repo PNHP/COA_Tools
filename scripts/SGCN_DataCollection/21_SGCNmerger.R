b7aebc0c0d5b89beaf20441741a477e729033811
Christoper Tracey <ctracey@paconserve.org>
2019-10-23 13:41:32 -0400

    minor code cleanup

--- scripts/SGCN_DataCollection/21_SGCNmerger.R
+++ scripts/SGCN_DataCollection/21_SGCNmerger.R
@@ -12,27 +12,22 @@
 # * 
 #---------------------------------------------------------------------------------------------
 # load packages
-if (!requireNamespace("arcgisbinding", quietly = TRUE)) install.packages("arcgisbinding")
-require(arcgisbinding)
 if (!requireNamespace("here", quietly = TRUE)) install.packages("here")
 require(here)
-if (!requireNamespace("sf", quietly = TRUE)) install.packages("sf")
-require(sf)
 if (!requireNamespace("rgdal", quietly = TRUE)) install.packages("rgdal")
 require(rgdal)
-if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr")
-require(dplyr)
 
-source(here::here("scripts","SGCN_DataCollection","00_PathsAndSettings.r"))
+source(here::here("scripts","00_PathsAndSettings.r"))
 
 sgcn_folder <- here::here("_data/output/SGCN.gdb")
 subset(ogrDrivers(), grepl("GDB", name))
 fc_list <- ogrListLayers(sgcn_folder)
 final_list <- fc_list[grepl("final",fc_list)]
+final_list # print out the final list
+
 
 columns <- c('OBJECTID','ELCODE','ELSeason','SNAME','SCOMNAME','SeasonCode','DataSource','DataID','OccProb','LastObs','useCOA','TaxaGroup')
 
-arc.check_product()
 data <- arc.open(path=here::here("_data/output/SGCN.gdb",final_list[1]))
 sgcn <- arc.select(data,columns)
 sgcn_sf <- arc.data2sf(sgcn)
