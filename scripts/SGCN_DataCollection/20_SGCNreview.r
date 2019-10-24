64ca29b852630d804cde48a77984816aa1ef9186
Christoper Tracey <ctracey@paconserve.org>
2019-10-24 16:22:58 -0400

    more edits to get everything to work

--- scripts/SGCN_DataCollection/20_SGCNreview.r
+++ scripts/SGCN_DataCollection/20_SGCNreview.r
@@ -34,6 +34,19 @@ lu_sgcnXpu <- dbGetQuery(db, statement = lu_sgcnXpu_SQLquery)
 lu_actions <- dbGetQuery(db, statement = lu_actionsLevel2_SQLquery)
 dbDisconnect(db) # disconnect the db
 
+## minor fixes
+lu_sgcnXpu[lu_sgcnXpu=="AAAAE01040_y"] <- "AAAAE01042_y"
+lu_sgcnXpu[lu_sgcnXpu=="AMAFB09020_y"] <- "AMAFB09030_y"
+lu_sgcnXpu[lu_sgcnXpu=="AMAFB09020_y"] <- "IILEP42010_y"
+lu_sgcnXpu[lu_sgcnXpu=="ABPBX03240_y"] <- "ABPBX03240_b"
+
+lu_sgcnXpu <- lu_sgcnXpu[which(lu_sgcnXpu$ELSeason!="ABNKD06020_y"&lu_sgcnXpu$ELSeason!="ABPBX03120_y"&lu_sgcnXpu$ELSeason!="ABPBX16010_y"&lu_sgcnXpu$ELSeason!="ABNTA07070_y"&lu_sgcnXpu$ELSeason!="ABPBK01010_y"&lu_sgcnXpu$ELSeason!="ABPBX01060_y"&lu_sgcnXpu$ELSeason!="ABPBXA4020_y"&lu_sgcnXpu$ELSeason!="ABNSB13040_y"&lu_sgcnXpu$ELSeason!="ABNNF19020_y"&lu_sgcnXpu$ELSeason!="ABPBA01010_y"&lu_sgcnXpu$ELSeason!="ABPBJ18100_y"&lu_sgcnXpu$ELSeason!="ABPBX05010_y"&lu_sgcnXpu$ELSeason!="ABPAU01010_y"&lu_sgcnXpu$ELSeason!="ABNNM08070_y"&lu_sgcnXpu$ELSeason!="ABPBY06030_y"&lu_sgcnXpu$ELSeason!="ABPBG10020_y"&lu_sgcnXpu$ELSeason!="ABPBX03050_y"&lu_sgcnXpu$ELSeason!="ABPBX03230_y"&lu_sgcnXpu$ELSeason!="ABNME05030_y"&lu_sgcnXpu$ELSeason!="ABNCA03010_y"&lu_sgcnXpu$ELSeason!="AMACC04010_y"&lu_sgcnXpu$ELSeason!="ABNME08020_y"&lu_sgcnXpu$ELSeason!="ABNKC12020_y"&lu_sgcnXpu$ELSeason!="ABNME13030"),]
+
+db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
+dbWriteTable(db, "lu_sgcnXpu_all", sgcnXpu_newTable, overwrite=TRUE) # write the table to the sqlite
+dbDisconnect(db) # disconnect the db
+
+
 # get number of lu_sgcn records, number of unique ELCODE values, and number of unique ELSeason values
 print(paste0("There are ",nrow(lu_sgcn)," rows in the master lu_sgcn list from the sqlite database."))
 print(paste0("There are ",length(unique(lu_sgcn$ELSeason))," unique ELSeason values in the master lu_sgcn list from the sqlite database."))
@@ -87,6 +100,8 @@ sgcn_punoaction <- setdiff(y,a)
 sgcn_recordnoaction <- subset(sgcn_punoaction,!(sgcn_punoaction %in% sgcn_norecord))
 print("The following ELSeason records are found in the lu_sgcnXpu and lu_sgcn tables, but do not have any corresponding actions: ")
 print(lu_sgcn[lu_sgcn$ELSeason %in% sgcn_recordnoaction,])
+write.csv(lu_sgcn[lu_sgcn$ELSeason %in% sgcn_recordnoaction,],"missing_actions.csv", row.names=FALSE)
+
 
 # get list of rows from actions table that include null or blank ELSeason values
 actions_null_ELCODE <- lu_actions[is.na(lu_actions$ELSeason)==TRUE,]
@@ -135,3 +150,13 @@ SGCNsummary <- SGCNsummary[order(SGCNsummary$TaxaDisplay,SGCNsummary$SCOMNAME,SG
 write.csv(SGCNsummary, "SGCNsummary.csv", row.names=FALSE)
 
                       
+
+
+
+
+
+######################
+#Fix trec data
+###lu_sgcnXpu_notrec <- lu_sgcnXpu[which(lu_sgcnXpu$)]
+
+
