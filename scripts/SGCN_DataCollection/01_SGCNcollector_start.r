9e7c8d77ac55dd033af20a8cd69fa1184e0b73e0
Christoper Tracey <ctracey@paconserve.org>
2020-04-06 10:30:28 -0400

    update path changes

--- scripts/SGCN_DataCollection/01_SGCNcollector_start.r
+++ scripts/SGCN_DataCollection/01_SGCNcollector_start.r
@@ -2,8 +2,8 @@ if (!requireNamespace("here", quietly = TRUE)) install.packages("here")
 require(here)
 
 # copy the blankSGCN directory from the base folder to the output directory
-current_folder <- here::here("_data/SGCN_blank.gdb") 
-new_folder <- here::here("_data/output/SGCN.gdb") 
+current_folder <- here::here("_data","SGCN_blank.gdb") 
+new_folder <- here::here("_data","output",updateName,"SGCN.gdb") 
 list_of_files <- list.files(path=current_folder, full.names=TRUE) 
 dir.create(new_folder)
 file.copy(from=file.path(list_of_files), to=new_folder,  overwrite=TRUE, recursive=FALSE, copy.mode=TRUE)
