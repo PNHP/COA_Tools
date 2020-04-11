58e49b1892611d918b3ee5cb886bc50a2ef5e395
Christoper Tracey <ctracey@paconserve.org>
2020-04-11 10:56:16 -0400

    change gdb template location and copy gdp for sws

--- scripts/RangeMaps/1_RangeUnit creator.r
+++ scripts/RangeMaps/1_RangeUnit creator.r
@@ -31,7 +31,12 @@ arc.check_product()
 
 # this script requires a geodatabase to be placed in the "" directory called "sws.gdb".  This gdb should two feature classes contained within it ("_huc08" and "_county")
 
-
+# copy the blankSGCN directory from the base folder to the output directory
+current_folder <- here::here("_data","templates","sws_blank.gdb") 
+new_folder <- here::here("_data","output",updateName,"sws.gdb") 
+list_of_files <- list.files(path=current_folder, full.names=TRUE) 
+dir.create(new_folder)
+file.copy(from=file.path(list_of_files), to=new_folder,  overwrite=TRUE, recursive=FALSE, copy.mode=TRUE)
 
 
 # # check to see if sws.gdb exists and create a new one
--- scripts/SGCN_DataCollection/01_SGCNcollector_start.r
+++ scripts/SGCN_DataCollection/01_SGCNcollector_start.r
@@ -8,7 +8,7 @@ source(here::here("scripts", "00_PathsAndSettings.r"))
 
 
 # copy the blankSGCN directory from the base folder to the output directory
-current_folder <- here::here("_data","SGCN_blank.gdb") 
+current_folder <- here::here("_data","templates","SGCN_blank.gdb") 
 new_folder <- here::here("_data","output",updateName,"SGCN.gdb") 
 list_of_files <- list.files(path=current_folder, full.names=TRUE) 
 dir.create(new_folder)
