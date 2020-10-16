66a144fd10a640709a1b3fc1fe2d2512b971d890
Christoper Tracey <ctracey@paconserve.org>
2020-10-16 13:14:04 -0400

    more tracking

--- scripts/2a_insertNaturalBoundaries.r
+++ scripts/2a_insertNaturalBoundaries.r
@@ -22,7 +22,7 @@ source(here::here("scripts", "00_PathsAndSettings.r"))
 
 ## Natural Boundaries
 NaturalBoundaries <- read.csv(here::here("_data","input","lu_NaturalBoundaries.csv"), stringsAsFactors=FALSE)
-#NaturalBoundaries <- CountyName[order(CountyName$COUNTY_NAM),]
+trackfiles("natural boundaries", here::here("_data","input","lu_NaturalBoundaries.csv")) # write to file tracker
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
 dbWriteTable(db, "lu_NaturalBoundaries", NaturalBoundaries, overwrite=TRUE) # write the table to the sqlite
 dbDisconnect(db) # disconnect the db
@@ -31,6 +31,7 @@ rm(NaturalBoundaries)
 ## HUC names
 HUCname <- read.csv(here::here("_data","input","lu_HUCname.csv"), stringsAsFactors=FALSE)
 HUCname <- HUCname[order(HUCname$HUC12name),]
+trackfiles("HUC names lookup", here::here("_data","input","lu_HUCname.csv")) # write to file tracker
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
 dbWriteTable(db, "lu_HUCname", HUCname, overwrite=TRUE) # write the table to the sqlite
 dbDisconnect(db) # disconnect the db
