dba9232e72061ee4d82589870389ec732ee6dba5
Christoper Tracey <ctracey@paconserve.org>
2020-10-16 13:49:52 -0400

    other rename and the bats that got in the list

--- scripts/06_ProtectedLandAgencies.r
+++ scripts/06_ProtectedLandAgencies.r
@@ -22,6 +22,7 @@ source(here::here("scripts", "00_PathsAndSettings.r"))
 
 ## Protected Land
 ProtectedLands <- read.csv(here::here("_data","input","lu_ProtectedLands_25.csv"), stringsAsFactors=FALSE)
+trackfiles("Protected Lands", here::here("_data","input","lu_ProtectedLands_25.csv")) # write to file tracker
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
   dbWriteTable(db, "lu_ProtectedLands_25", ProtectedLands, overwrite=TRUE) # write the table to the sqlite
 dbDisconnect(db) # disconnect the db
@@ -29,6 +30,7 @@ rm(ProtectedLands)
 
 ## Agency Districts
 AgencyDistricts <- read.csv(here::here("_data","input","lu_AgencyDistricts.csv"), stringsAsFactors=FALSE)
+trackfiles("AgencyDistricts", here::here("_data","input","lu_AgencyDistricts.csv")) # write to file tracker
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
   dbWriteTable(db, "lu_AgencyDistricts", AgencyDistricts, overwrite=TRUE) # write the table to the sqlite
 dbDisconnect(db) # disconnect the db
@@ -36,6 +38,7 @@ rm(AgencyDistricts)
 
 ## PGC Regions
 PGC_RegionName <- read.csv(here::here("_data","input","lu_PGC_RegionName.csv"), stringsAsFactors=FALSE)
+trackfiles("PGC Regions", here::here("_data","input","lu_PGC_RegionName.csv")) # write to file tracker
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
 dbWriteTable(db, "lu_PGC_RegionName", PGC_RegionName, overwrite=TRUE) # write the table to the sqlite
 dbDisconnect(db) # disconnect the db
