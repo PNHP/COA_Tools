9800d077416740bf082472c58b5f9e2dab368bdf
Christoper Tracey <ctracey@paconserve.org>
2021-01-26 14:44:32 -0500

    fixed a bunch of errors related to the ET as a service

--- scripts/02_insertSGCN.r
+++ scripts/02_insertSGCN.r
@@ -21,9 +21,9 @@ source(here::here("scripts", "00_PathsAndSettings.r"))
 
 SGCNlist_file <- list.files(path=here::here("_data","input"), pattern="^lu_SGCN")  # --- make sure your excel file is not open.
 SGCNlist_file
-#look at the output and choose which shapefile you want to run
+#look at the output and choose which file you want to run
 #enter its location in the list (first = 1, second = 2, etc)
-n <- 3
+n <- 1 # this should "lu_SGCN.csv"
 SGCNlist_file <- here::here("_data","input",SGCNlist_file[n])
 SGCN <- read.csv(SGCNlist_file, stringsAsFactors=FALSE)
 
@@ -57,11 +57,19 @@ ET <- arc.select(ET, c("ELSUBID","ELCODE","SNAME","SCOMNAME","GRANK","SRANK","SR
 
 SGCNtest <- merge(SGCN[c("ELCODE","SNAME","SCOMNAME","GRANK","SRANK")], ET, by.x="ELCODE", by.y="ELCODE", all.x = TRUE)
 
-SGCNtest$matchGRANK <- ifelse(SGCNtest$GRANK==SGCNtest$G.RANK,"yes","no")
-SGCNtest$matchSRANK <- ifelse(SGCNtest$SRANK==SGCNtest$S.RANK,"yes","no")
+SGCNtest$matchGRANK <- ifelse(SGCNtest$GRANK.x==SGCNtest$GRANK.y,"yes","no")
+if(is.null(SGCNtest[which(SGCNtest$matchGRANK=="no"),]$GNAME)){
+  print("GRANK strings match. You're good to go!")
+} else {
+  print(paste("GRANKS for ", SGCNtest[which(SGCNtest$matchGRANK=="no"),]$GNAME , " do not match;", sep=""))
+}
 
-SGCNtest[which(SGCNtest$matchGRANK=="no"),]$SNAME
-SGCNtest[which(SGCNtest$matchSRANK=="no"),]$SNAME
+SGCNtest$matchSRANK <- ifelse(SGCNtest$SRANK.x==SGCNtest$SRANK.y,"yes","no")
+if(is.null(SGCNtest[which(SGCNtest$matchSRANK=="no"),]$SNAME)){
+  print("GRANK strings match. You're good to go!")
+} else {
+  print(paste("SRANKS for ", SGCNtest[which(SGCNtest$matchSRANK=="no"),]$SNAME , " do not match;", sep=""))
+}
 
 SGCNtest <- SGCNtest[which(SGCNtest$matchGRANK=="no"|SGCNtest$matchSRANK=="no"),] # edit this down to just the changes
 names(SGCNtest) <- c("ELCODE","SGCN_SNAME","SGCN_SCOMNAME","SGCN_GRANK","SGCN_SRANK","ET_SNAME","ET_SCOMNAME","ET_GRANK","ET_SRANK","ET_SRANK.CHANGE.DATE","ET_SRANK.REVIEW.DATE","matchGRANK","matchSRANK") 
@@ -74,13 +82,15 @@ dbAppendTable(dbTracking, "changed_ranks", SGCNtest, overwrite=TRUE) # write the
 dbDisconnect(dbTracking) # disconnect the db
 
 # replace data in table
-SGCN1 <- merge(SGCN, ET[c("ELCODE","G.RANK","S.RANK")], by="ELCODE", all.x = TRUE )
-SGCN1$GRANK <- SGCN1$G.RANK
-SGCN1$G.RANK <- NULL
-SGCN1$SRANK <- SGCN1$S.RANK
-SGCN1$S.RANK <- NULL
+SGCN1 <- merge(SGCN, ET[c("ELCODE","GRANK","SRANK")], by="ELCODE", all.x = TRUE )
+SGCN1$GRANK.x <- SGCN1$GRANK.y
+SGCN1$GRANK.y <- NULL
+names(SGCN1)[names(SGCN1) == "GRANK.x"] <- "GRANK"
+SGCN1$SRANK.y <- SGCN1$SRANK.y
+SGCN1$SRANK.y <- NULL
+names(SGCN1)[names(SGCN1) == "SRANK.x"] <- "SRANK"
 SGCN <- SGCN1
-
+rm(SGCN1)
 write.csv(SGCN, here::here("_data","input",paste("lu_SGCN",updateName,".csv", sep="")), row.names=FALSE)
 
 ###########################################
