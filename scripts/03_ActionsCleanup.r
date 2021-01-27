1b3eeaeb3e3fc7d3cf21e23c52b554352953348f
Christoper Tracey <ctracey@paconserve.org>
2021-01-26 23:10:34 -0500

    lots of changes

--- scripts/03_ActionsCleanup.r
+++ scripts/03_ActionsCleanup.r
@@ -31,7 +31,7 @@ COA_actions_file <- list.files(path=here::here("_data/input"), pattern=".xlsx$")
 COA_actions_file
 #look at the output and choose which shapefile you want to run
 #enter its location in the list (first = 1, second = 2, etc)
-n <- 1
+n <- 4
 COA_actions_file <- here::here("_data/input",COA_actions_file[n])
 
 # write to file tracker
